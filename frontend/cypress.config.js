932baba7e6e98d7277b3437d7cae678195d3ef22
araxia55 <105526060+araxia55@users.noreply.github.com>
2024-12-06 13:21:34 +1300

    Release/workflow: update frontend deployment (#11)
    * Update github-actions-demo.yml
    
    * crc: deploy-frontend
    
    * crc: update aws-region
    
    * crc: update readme
    
    * release: update workflow
    
    * release: update workflow with tests
    
    * workflow: add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix error
    
    * workflow: fix error
    
    * workflow: fix error
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * workflow: fix and add test
    
    * release/workflow: fix project structure
    
    * remove files
    
    * move readme
    
    * fix spacing
    
    * cleanup extra readme
    
    * add workflow emoji
    
    * add workflow emoji
    
    * add workflow emoji
    
    * fix readme
    
    * move workflow file
    
    * fix workflow file
    
    * fix workflow file
    
    * fix workflow file
    
    * fix workflow file
    
    * fix workflow file
    
    * fix workflow file
    
    * move cypress files
    
    * fix cypress config file
    
    * fix cypress config file
    
    * fix cypress config file
    
    * fix cypress config file
    
    * fix cypress config file
    
    * fix deploy workflow
    
    * fix deploy workflow
    
    * fix deploy workflow
    
    * fix deploy workflow
    
    * fix proj structure and  workflow
    
    * fix proj structure and  workflow
    
    * fix workflow
    
    * including pnpm version
    
    * including pnpm version
    
    * including pnpm version
    
    * including pnpm install step
    
    * including pnpm install step
    
    * including pnpm install step
    
    * including pnpm install step
    
    * fix pnpm install step
    
    * fix pnpm install step
    
    * fix pnpm install step
    
    * fix pnpm install step
    
    * fix tsconfig location
    
    * fix tsconfig and test summary
    
    * fix tsconfig location
    
    * cleanup yml file
    
    * add code formatter
    
    * fix cypress config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     supportFile: false,
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     // setup baseUrl for convenience
-    baseUrl: 'http://localhost:4321/', 
+    baseUrl: 'http://localhost:4321', 
   }
 })
--- frontend/cypress/e2e/features/index.cy.js
+++ frontend/cypress/e2e/features/index.cy.js
@@ -1,5 +1,5 @@
 it('titles are correct', () => {
-    const page = cy.visit("/");
+    const page = cy.visit("/", {timeout: 10000 });
   
     page.get('title').should('have.text', 'Ray')
     page.get('h1').should('have.text', "\nHello, I'm Ray.\n");
